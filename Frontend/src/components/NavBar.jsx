8ae8d64eab5e7452e3b9dbf0d00e21eae4e71f89
Sha Hos <shossain43@gatech.edu>
2024-11-29 01:45:25 -0500

    Added page links to navbar

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -24,10 +24,13 @@ function Navbar({ className }) {
           loc="/usermanagement"
         >
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/addowner">Add Owner</HoveredLink>
+            <HoveredLink href="/addemployee">Add Employee</HoveredLink>
+            <HoveredLink href="/addworker">Add Worker</HoveredLink>
+            <HoveredLink href="/adddriver">Add Driver</HoveredLink>
+            <HoveredLink href="/hireemployee">Hire Employee</HoveredLink>
+            <HoveredLink href="/fireemployee">Fire Employee</HoveredLink>
+            <HoveredLink href="/removedriver">Remove Driver</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem
@@ -37,36 +40,38 @@ function Navbar({ className }) {
           loc="/vanmanagement"
         >
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/addvan">Add Van</HoveredLink>
+            <HoveredLink href="/takeovervan">Take Over Van</HoveredLink>
+            <HoveredLink href="/drivervan">Drive Van</HoveredLink>
+            <HoveredLink href="/loadvan">Load Van</HoveredLink>
+            <HoveredLink href="/refuelvan">Refuel Van</HoveredLink>
+            <HoveredLink href="/removevan">Remove Van</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem
           setActive={setActive}
           active={active}
           item="Company Management"
-          loc="companymanagement"
+          loc="/companymanagement"
         >
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            <HoveredLink href="/addservice">Add Service</HoveredLink>
+            <HoveredLink href="/addbusiness">Add Business</HoveredLink>
+            <HoveredLink href="/addlocation">Add Location</HoveredLink>
+            <HoveredLink href="/fundbusiness">Fund Business</HoveredLink>
+            <HoveredLink href="/manageservice">Manage Service</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem
           setActive={setActive}
           active={active}
-          item="Product Mangement"
-          loc="productmanagement"
+          item="Product Management"
+          loc="/productmanagement"
         >
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/addproduct">Add Product</HoveredLink>
+            <HoveredLink href="/purchaseproduct">Purchase Product</HoveredLink>
+            <HoveredLink href="/removeproduct">Remove Product</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem
@@ -76,10 +81,12 @@ function Navbar({ className }) {
           loc="/displays"
         >
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/ownerview">Owner View</HoveredLink>
+            <HoveredLink href="/employeeview">Employee View</HoveredLink>
+            <HoveredLink href="/serviceview">Service View</HoveredLink>
+            <HoveredLink href="/driverview">Driver View</HoveredLink>
+            <HoveredLink href="/locationview">Location View</HoveredLink>
+            <HoveredLink href="/productview">Product View</HoveredLink>
           </div>
         </MenuItem>
       </Menu>
