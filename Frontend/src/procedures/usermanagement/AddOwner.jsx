2c32da914e22add21b57c9cc70851c9522287570
Sha Hos <shossain43@gatech.edu>
2024-11-30 22:09:27 -0500

    Fixed heighting of forms

--- Frontend/src/procedures/usermanagement/AddOwner.jsx
+++ Frontend/src/procedures/usermanagement/AddOwner.jsx
@@ -13,7 +13,7 @@ const AddOwner = () => {
 
   return (
     <div className="flex justify-center items-center h-screen bg-[#F3F1E5]">
-      <div className="h-1/3 w-1/3 p-6 rounded-md border border-black bg-white text-black shadow-[4px_4px_0px_0px_rgba(0,0,0)]">
+      <div className="h-3/3 w-1/3 p-6 rounded-md border border-black bg-white text-black shadow-[4px_4px_0px_0px_rgba(0,0,0)]">
         <h1 className="text-2xl font-bold mb-4">ADD_OWNER()</h1>
 
         <div className="mb-6">
